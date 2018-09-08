89d0105f31c6b8291c7aa7a2afb93e56b6d4b262
LISANDROLAN <LISANDROLAN@GMAIL.COM>
2018-09-08 19:43:22 -0300

    qrcode ready

--- src/App.js
+++ src/App.js
@@ -4,49 +4,36 @@ import './App.css';
 
 import jsPDF from 'jspdf';
 import 'jspdf-autotable';
+import QRCode from 'qrcode.react'
 import ReactDOMServer from "react-dom/server"
-
+import canvg from 'canvg'
 class App extends Component {
   printDocument() {
-  
+
     const pdf = new jsPDF();
     //   pdf.addImage(imgData, 'JPEG', 0, 0);   // pdf.output('dataurlnewwindow');
-    var estatico = <div
-      id="divToPrint"
-      class="mt4"
-      style={{
-      backgroundColor: '#f5f5f5',
-      width: '210mm',
-      minHeight: '297mm',
-      marginLeft: 'auto',
-      marginRight: 'auto'
-    }}>
-      <div>TEXTO QUE NO SE VE</div>
-      <div>You Can add any component here</div>
-    </div>
-    pdf.fromHTML(ReactDOMServer.renderToStaticMarkup(estatico))
-    pdf.save("download.pdf");
-    // });
+    var svg = ReactDOMServer.renderToStaticMarkup(<QRCode renderAs='svg' value='baf19619-b38a-11e8-ae0b-061cacc9b2a2'/>);
+    svg = svg
+      .replace(/\r?\n|\r/g, '')
+      .trim();
+    var canvas = document.createElement('canvas');
+    canvg(canvas, svg);
+    var imgData = canvas.toDataURL('image/png');
+    console.log(canvas)
+    // pdf.fromHTML(ReactDOMServer.renderToStaticMarkup(estatico))
+    pdf.addImage(imgData, 'PNG', 40, 40, 75, 75);
+    pdf.save('test.pdf');
   }
+
+  // });
+
   render() {
     return (
       <div>
         <div className="mb5">
           <button onClick={this.printDocument}>Print</button>
         </div>
-        <div
-          id="divToPrint"
-          className="mt4"
-          style={{
-          backgroundColor: '#f5f5f5',
-          width: '210mm',
-          minHeight: '297mm',
-          marginLeft: 'auto',
-          marginRight: 'auto'
-        }}>
-          <div>Note: Here the dimensions of div are same as A4</div>
-          <div>You Can add any component here</div>
-        </div>
+
       </div>
     );
   }
